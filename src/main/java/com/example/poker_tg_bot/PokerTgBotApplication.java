6563e8afa710d8c634610ef3600ed31af45ff0c9
ovoskresenskyi <7949894@ukr.net>
2023-02-16 10:35:08 +0200

    Init commit

---
+++ src/main/java/com/example/poker_tg_bot/PokerTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.poker_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerTgBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerTgBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/poker_tg_bot/PokerTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.poker_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerTgBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
