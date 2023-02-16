6e9a1e326d427b44f58407f7deaeda2b3e08b949
ovoskresenskyi <7949894@ukr.net>
2023-02-16 23:22:02 +0200

    Added model of Bot with extending of LongPollingBot
    Need to figure out with deprecated methods and constructor

---
+++ src/main/java/com/example/poker_tg_bot/model/Bot.java
@@ -0,0 +1,60 @@
+package com.example.poker_tg_bot.model;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Slf4j
+@Component
+public class Bot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botUsername;
+
+    @Value("${bot.token}")
+    private String botToken;
+
+    private final UpdateReceiver updateReceiver;
+
+    @Autowired
+    public Bot(UpdateReceiver updateReceiver) {
+        this.updateReceiver = updateReceiver;
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        List<PartialBotApiMethod<? extends Serializable>> messagesToSend = updateReceiver.handle(update);
+
+        if (messagesToSend != null && !messagesToSend.isEmpty()) {
+            messagesToSend.forEach(response -> {
+                if (response instanceof SendMessage) {
+                    executeWithExceptionCheck((SendMessage) response);
+                }
+            });
+        }
+    }
+
+    public void executeWithExceptionCheck(SendMessage sendMessage) {
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("oops");
+        }
+    }
+
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+}
